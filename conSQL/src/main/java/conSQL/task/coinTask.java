1cb5527d7e2943e7400bcd3e252fda450c4e1c42
tfhiroto0906 <79963338+tfhiroto0906@users.noreply.github.com>
2021-03-17 16:17:40 +0900

    Add files via upload

--- conSQL/src/main/java/conSQL/task/coinTask.java
+++ conSQL/src/main/java/conSQL/task/coinTask.java
@@ -9,12 +9,14 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 import conSQL.SQLMain;
+import net.md_5.bungee.api.ChatColor;
 
 public class coinTask {
 	static SQLMain plugin = SQLMain.getPlugin(SQLMain.class);
 	static FileConfiguration config = plugin.getConfig();
 	String[] option = new String[3];
 	Player player;
+
 	public static int getCoin(Player pl) {
 		UUID uuid = pl.getUniqueId();
 		PreparedStatement statement;
@@ -33,11 +35,17 @@ public class coinTask {
 	}
 
 	//playerのCOINSを指定された値にupdate
-	public static void updateCoins(int setcoins,Player pl,boolean b) {
+	public static boolean updateCoins(int setcoins,Player pl,boolean b) {
 		UUID uuid = pl.getUniqueId();
+		int incoin=setcoins;
 		//加算処理の時
 		if(b) {
 			setcoins +=getCoin(pl);
+			if(setcoins<0) {
+				pl.sendMessage(ChatColor.DARK_RED + "coinが足りません");
+				return false;
+			}
+			pl.sendMessage(ChatColor.YELLOW + "" + incoin +"Coin");
 		}
 		try {
 			PreparedStatement statement = plugin.getConnection()
@@ -48,6 +56,7 @@ public class coinTask {
 		}catch (SQLException e) {
 			e.printStackTrace();
 		}
+		return true;
 	}
 }
 
